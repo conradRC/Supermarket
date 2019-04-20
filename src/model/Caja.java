af9ee8a0522d3a1907913ee03453233501f625ec
Conrado Cruz <conrado27cym@gmail.com>
2019-04-20 05:05:17 -0500

    Agregado modelo de Cajas

---
+++ src/model/Caja.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Queue;
+public class Caja extends Thread {
+    private String nombre;
+    private Queue<Cliente> cliente;
+    private long initialTime;
+    public Caja(String nombre, Queue <Cliente> cliente, long initialTime) {
+        this.nombre = nombre;
+        this.cliente = cliente;
+        this.initialTime = initialTime;
+    }
+
+    public long getInitialTime() {
+        return initialTime;
+    }
+
+    public void setInitialTime(long initialTime) {
+        this.initialTime = initialTime;
+    }
+
+    public Queue<Cliente> getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public void run() {
+       System.out.println("Inicial Time: "+ (System.currentTimeMillis() - initialTime) / 1000);
+       int total=0;
+       while(!Principal.clientes.isEmpty()){
+          Cliente client = Principal.clientes.poll();
+          for(int c =0 ; c<Principal.carro1.size(); c++){
+               timeWait(client.getCarroCompra().get(c));
+              System.out.println("Productos: " + client.getCarroCompra().get(c) +
+                                 " :  cliente: " + client.getNombre() + "->Tiempo: "+ 
+                              (System.currentTimeMillis() - initialTime) / 1000+ "s");
+              total+= client.getCarroCompra().get(c);
+           }
+           System.out.println("cliente: " +client.getNombre() +"  Total productos : " + total);
+           //System.out.println(nombre + "-> TERMINÓ " + Principal.clientes.get(c).getNombre() + "-> TIME ALL: "+
+            //                (System.currentTimeMillis() - initialTime) / 1000+ "s");
+       }
+    }
+
+    private void timeWait(byte segundos) {
+        try {
+           Thread.sleep(segundos * 1000);
+        } catch (InterruptedException ex) {
+           Thread.currentThread().interrupt();
+        }
+    }
+
+    /* System.out.println( nombre + " con el cliente " 
+                    + cliente.getNombre() + " EN EL TIEMPO: " 
+                    + time+ "s");*/
+}
